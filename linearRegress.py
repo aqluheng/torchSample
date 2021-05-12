ba7c787dc93f426cf2c2f3de53e456130c3a44d9
aqluheng <283440992@qq.com>
2021-05-12 16:33:53 +0800

    init

---
+++ linearRegress.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+class LinearModel(nn.Module):
+    def __init__(self, ndim):
+        super(LinearModel, self).__init__()
+        self.ndim = ndim
+
+        self.weight = nn.Parameter(torch.randn(ndim,1))
+        self.bias = nn.Parameter(torch.randn(1))
+
+    def forward(self,x):
+        # y = Wx + b
+        return x.mm(self.weight) + self.bias
+
+
+lm = LinearModel(5)
+x = torch.randn(4,5)
+print(lm(x))
+
+        
